dd3275cf4350c3570c204505ed4c51d5dffcda7e
SFERAS <rthyssen73@gmail.com>
2024-07-10 07:53:06 +0200

    updated root

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,54 +15,54 @@ const tokenFromLS = localStorage.getItem("token");
 const tokenDefault = tokenFromLS ? tokenFromLS : null;
 
 export default function App() {
-  const [user, setUser] = useState(userDefault);
-  const [token, setToken] = useState(tokenDefault);
-  const [userProfPic, setUserProfPic] = useState(userProfPicDefault);
-  const { theme, setLatitude, setLongitude } = useContext(Context);
+	const [user, setUser] = useState(userDefault);
+	const [token, setToken] = useState(tokenDefault);
+	const [userProfPic, setUserProfPic] = useState(userProfPicDefault);
+	const { theme, setLatitude, setLongitude } = useContext(Context);
 
-  useEffect(() => {
-    if (user) {
-      localStorage.setItem("user", JSON.stringify(user));
-    } else {
-      localStorage.removeItem("user");
-    }
-  }, [user]);
+	useEffect(() => {
+		if (user) {
+			localStorage.setItem("user", JSON.stringify(user));
+		} else {
+			localStorage.removeItem("user");
+		}
+	}, [user]);
 
-  useEffect(() => {
-    if (token) {
-      localStorage.setItem("token", token);
-    } else {
-      localStorage.removeItem("token");
-    }
-  }, [token]);
+	useEffect(() => {
+		if (token) {
+			localStorage.setItem("token", token);
+		} else {
+			localStorage.removeItem("token");
+		}
+	}, [token]);
 
-  useEffect(() => {
-    if (userProfPic) {
-      localStorage.setItem("userProfPic", userProfPic);
-    } else {
-      localStorage.removeItem("userProfPic");
-    }
-  }, [userProfPic]);
+	useEffect(() => {
+		if (userProfPic) {
+			localStorage.setItem("userProfPic", userProfPic);
+		} else {
+			localStorage.removeItem("userProfPic");
+		}
+	}, [userProfPic]);
 
-  return (
-    <div className={theme}>
-      {user && token ? (
-        <>
-          <Header setUser={setUser} setToken={setToken}
-            setUserProfPic={setUserProfPic}
-          />
-          <Main
-            setUser={setUser} user={user} setToken={setToken}
-            token={token} userProfPic={userProfPic}
-            setUserProfPic={setUserProfPic}
-          />
-        </>
-      ) : (
-        <Log
-          setUser={setUser} setToken={setToken}
-          setUserProfPic={setUserProfPic}
-        />
-      )}
-    </div>
-  );
+	return (
+		<div className={theme}>
+			{user && token ? (
+				<>
+					<Header setUser={setUser} setToken={setToken}
+						setUserProfPic={setUserProfPic}
+					/>
+					<Main
+						setUser={setUser} user={user} setToken={setToken}
+						token={token} userProfPic={userProfPic}
+						setUserProfPic={setUserProfPic}
+					/>
+				</>
+			) : (
+				<Log
+					setUser={setUser} setToken={setToken}
+					setUserProfPic={setUserProfPic}
+				/>
+			)}
+		</div>
+	);
 }
--- frontend/src/Main.jsx
+++ frontend/src/Main.jsx
@@ -4,52 +4,52 @@ import Profile from "./Routes/Profile"
 import Chats from "./Routes/Chats"
 import axios from "axios";
 import React, { useEffect } from "react";
-import {Routes,Route} from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 import { useContext } from "react";
 import { Context } from "./components/context.js"
 
-export default function Main(props){
-    const {setIsNewMessageCame} = useContext(Context)
-    function loadChats(){
-        const headers = { Authorization: `Bearer ${props.token}` }
-        axios.get(`${process.env.REACT_APP_BE_SERVER}/chats`, {headers})
-            .then (res=>{
-                let helper=false
-                res.data.map(item=>{
-                    if(!item.redBy.includes(props.user)){
-                        helper=true
-                    }
-                })
-                setIsNewMessageCame(helper)
-            })
-            .catch(error => {
-                if(error){console.log(error)}
-                if(error.response.data.error.message=="jwt expired"){
-                    localStorage.removeItem("token")
-                    props.setToken(null)
-                }
-            })
-    }
-    
-    useEffect(()=>{
-        const id=setInterval(loadChats,5000)
-        return ()=>clearInterval(id)
-    },[])
+export default function Main(props) {
+	const { setIsNewMessageCame } = useContext(Context)
+	function loadChats() {
+		const headers = { Authorization: `Bearer ${props.token}` }
+		axios.get(`${process.env.REACT_APP_BE_SERVER}/chats`, { headers })
+			.then(res => {
+				let helper = false
+				res.data.map(item => {
+					if (!item.redBy.includes(props.user)) {
+						helper = true
+					}
+				})
+				setIsNewMessageCame(helper)
+			})
+			.catch(error => {
+				if (error) { console.log(error) }
+				if (error.response.data.error.message == "jwt expired") {
+					localStorage.removeItem("token")
+					props.setToken(null)
+				}
+			})
+	}
 
-    return(
-        <main>
-            <Routes>
-                <Route path="*" element={<Start user= {props.user} setToken={props.setToken} token= {props.token} /> }/>
-                <Route path="Search" element={<Search user= {props.user} setToken={props.setToken} token= {props.token}/>}/>
-                <Route path="Chats/*" element={<Chats user= {props.user} setToken={props.setToken} token= {props.token}/>}/>
-                <Route path="Profile" element={<Profile
-                    setToken={props.setToken}
-                    setUser={props.setUser} 
-                    userProfPic={props.userProfPic} 
-                    setUserProfPic={props.setUserProfPic} 
-                    user= {props.user} 
-                    token= {props.token}/> }/>
-            </Routes>
-        </main>
-    )
+	useEffect(() => {
+		const id = setInterval(loadChats, 5000)
+		return () => clearInterval(id)
+	}, [])
+
+	return (
+		<main>
+			<Routes>
+				<Route path="*" element={<Start user={props.user} setToken={props.setToken} token={props.token} />} />
+				<Route path="Search" element={<Search user={props.user} setToken={props.setToken} token={props.token} />} />
+				<Route path="Chats/*" element={<Chats user={props.user} setToken={props.setToken} token={props.token} />} />
+				<Route path="Profile" element={<Profile
+					setToken={props.setToken}
+					setUser={props.setUser}
+					userProfPic={props.userProfPic}
+					setUserProfPic={props.setUserProfPic}
+					user={props.user}
+					token={props.token} />} />
+			</Routes>
+		</main>
+	)
 }
